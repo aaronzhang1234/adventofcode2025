74008d7ab4e7772cfdacf4d63ff41baa46bb535e
aaronzhang1234 <aaronzhang.xyz@gmail.com>
2025-12-07 14:15:30 -0600

    day5 pt1

---
+++ day5/day5.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+
+func main(){
+	file, _ := os.Open("DB.txt")
+	scanner:= bufio.NewScanner(file)
+	var db [][]int
+	for scanner.Scan(){
+	    memes := strings.Split(scanner.Text(), "-")
+		min, _ := strconv.Atoi(memes[0])
+		max, _ := strconv.Atoi(memes[1])
+		db = append(db,[]int{min, max})
+	}	
+
+	stockFile, _ := os.Open("stocklist.txt")
+	stockScanner:= bufio.NewScanner(stockFile)
+	numFresh :=0
+	for stockScanner.Scan(){
+		stockListNum,_ := strconv.Atoi(stockScanner.Text())
+		for i:=0; i<len(db); i++{
+			if stockListNum>= db[i][0] && stockListNum<= db[i][1]{
+				numFresh = numFresh +1
+				break
+			}
+		}
+	}
+	fmt.Println(numFresh)
+
+}
