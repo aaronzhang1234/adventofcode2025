12ed050d7c59ad8886a4840f260125198c9ac533
aaronzhang1234 <aaronzhang.xyz@gmail.com>
2025-12-07 14:59:48 -0600

    pt2 day5

--- day5/day5.go
+++ day5/day5.go
@@ -1,13 +1,14 @@
 package main
 
 import (
+	"cmp"
 	"bufio"
 	"fmt"
 	"os"
 	"strconv"
 	"strings"
+	"slices"
 )
-
 
 func main(){
 	file, _ := os.Open("DB.txt")
@@ -19,19 +20,34 @@ func main(){
 		max, _ := strconv.Atoi(memes[1])
 		db = append(db,[]int{min, max})
 	}	
-
-	stockFile, _ := os.Open("stocklist.txt")
-	stockScanner:= bufio.NewScanner(stockFile)
-	numFresh :=0
-	for stockScanner.Scan(){
-		stockListNum,_ := strconv.Atoi(stockScanner.Text())
-		for i:=0; i<len(db); i++{
-			if stockListNum>= db[i][0] && stockListNum<= db[i][1]{
-				numFresh = numFresh +1
-				break
-			}
+	slices.SortFunc(db, CompareRanges)
+	for i :=0; i<len(db)-1;i++{
+		a,b := db[i], db[i+1]
+		if (a[0] == b[0]) || (b[0]>=a[0] && b[0]<=a[1]){
+			db[i] =	combineRanges(a,b)
+			db = append(db[:i+1], db[i+2:]...)
+			i = i-1
 		}
 	}
-	fmt.Println(numFresh)
+	totalFresh := 0
+	for i :=0; i<len(db);i++{
+		totalFresh = totalFresh + db[i][1]-db[i][0] + 1
+	}
+	fmt.Println(totalFresh)
+}
+func CompareRanges(a,b[]int) int{
+	if a[0] == b[0]{
+		return cmp.Compare(a[1], b[1])
+	}
+	return cmp.Compare(a[0], b[0])
+}
 
+func combineRanges(a,b []int) []int{
+	if a[0]==b[0] && a[1] == b[1]{ //same range
+		return []int{a[0], a[1]}
+	}
+	if b[1] > a[1]{
+		return []int{a[0],b[1]}
+	}
+	return []int{a[0], a[1]}
 }
