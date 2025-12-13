d19bde06e8364880fed7118b747606cb195f64ba
aaronzhang1234 <aaronzhang.xyz@gmail.com>
2025-12-13 14:10:49 -0600

    day7pt2

--- day7/day7.go
+++ day7/day7.go
@@ -9,29 +9,37 @@ import (
 func main(){
 	file, _ := os.Open("input.txt")
 	scanner:= bufio.NewScanner(file)
-	var beamLocations []string
-	split :=0
+	var beamLocations []int
+	var nextBeamLocations []int
 	for scanner.Scan(){
 		text := scanner.Text()
 		if beamLocations == nil{
 			for i:=0; i<len(text); i++{
 				if string(text[i])=="S"{
-					beamLocations = append(beamLocations, "|")
+					beamLocations = append(beamLocations, 1)
 				}else{
-					beamLocations = append(beamLocations, ".")
+					beamLocations = append(beamLocations, 0)
 				}	 
 			}
 		}else{
+			nextBeamLocations = beamLocations
 			for i:=0; i<len(text); i++{
-				if string(text[i]) == "^" && beamLocations[i] == "|"{
-					beamLocations[i-1] = "|"
-					beamLocations[i+1] = "|"
-					beamLocations[i] = "."
-					split = split + 1
+				if string(text[i]) == "^" && beamLocations[i] != 0{
+					nextBeamLocations[i-1] =+ nextBeamLocations[i-1]+beamLocations[i]
+					nextBeamLocations[i+1] =+ nextBeamLocations[i+1]+beamLocations[i]
+					nextBeamLocations[i] = 0
+				}else{
+					nextBeamLocations[i] = beamLocations[i]
 				}
 			}
+			fmt.Println(nextBeamLocations)
+			beamLocations = nextBeamLocations
 		}
 		fmt.Println(beamLocations)
 	}	
-	fmt.Println(split)
+	total := 0
+	for x:=0; x<len(beamLocations); x++{
+		total = total + beamLocations[x]
+	}
+	fmt.Println(total)
 }
